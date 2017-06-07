aad12e3a5528fd6fba8262b74d8fc42effd38a18
Tanzim Mokammel <mtanzim@gmail.com>
2017-06-06 22:05:23 -0400

    updated localWeather

--- localWeather/getWeather.js
+++ localWeather/getWeather.js
@@ -12,9 +12,41 @@ $(document).ready(function(){
 			isCel=0;
 		}
 	};
+
+
+	function getWeather () {
+		$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
+	  		$("#weatherInfo").html(JSON.stringify(json));
+		});
+	};
+
+	/*
+	function getWeather () {
+		$.ajax({
+		  url: "api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test",
+		  jsonp: "callback",
+		  type: "GET",
+		  dataType: "jsonp",
+		  success: function (data) {
+			//$quote = data["value"]["joke"];
+			//$("#quote").html($quote);
+			//$("#author").html("-" + $author);
+			$temp = data[]["temp"];
+			$("#weatherInfo").html($temp);
+
+
+		  },
+		  xhrFields: {
+			withCredentials: false
+		  }
+		});
+	};
+	*/
 	
+	//getWeather();
 
 	toggleUnit("#togBtn");
+	getWeather();
 	
 	$("#togBtn").on("click", function(){
 		toggleUnit ((this));
