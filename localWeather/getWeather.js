299763adb81ee133e5b52d35bcc79b10ff577d18
Tanzim Mokammel <mtanzim@gmail.com>
2017-06-08 01:19:50 -0400

    got OpenWeatherAPI JSON working

--- localWeather/getWeather.js
+++ localWeather/getWeather.js
@@ -1,55 +1,52 @@
 $(document).ready(function(){
 	
-	var tempUnit = ["C", "F"];
+	var tempUnit = ["metric", "imperial","C","F"];
 	var isCel = 0;
+	var lat=0;
+	var longt=0;
+	var owmAPI ="b0c19d689944d2473d76de9f1cc4339f";
 	
 	function toggleUnit (divInfo) {
 		if (isCel===0) {
-			$(divInfo).html(tempUnit[isCel]);
+			//$(divInfo).html(tempUnit[isCel]);
 			isCel=1;
 		} else {
-			$(divInfo).html(tempUnit[isCel]);
+			//$(divInfo).html(tempUnit[isCel]);
 			isCel=0;
 		}
 	};
 
 
 	function getWeather () {
-		$.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
-	  		$("#weatherInfo").html(JSON.stringify(json));
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+longt+"&APPID="+owmAPI+"&units="+tempUnit[isCel], function(json) {
+	  		var html = '';
+	  		//html+=JSON.stringify(json)+"<br><br>";
+	  		html+=json["name"]+"<br>";
+	  		html+=json["weather"][0]["main"]+"<br>";
+	  		html+=json["main"]["temp"]+" "+tempUnit[isCel+2]+"<br>";
+	  		$("#weatherInfo").html(html);
+	  		/*
+	  		var relevantWeather = {
+	  			name = json["name"];
+	  		};
+	  		*/
 		});
 	};
 
-	/*
-	function getWeather () {
-		$.ajax({
-		  url: "api.openweathermap.org/data/2.5/weather?q=London,uk&callback=test",
-		  jsonp: "callback",
-		  type: "GET",
-		  dataType: "jsonp",
-		  success: function (data) {
-			//$quote = data["value"]["joke"];
-			//$("#quote").html($quote);
-			//$("#author").html("-" + $author);
-			$temp = data[]["temp"];
-			$("#weatherInfo").html($temp);
-
-
-		  },
-		  xhrFields: {
-			withCredentials: false
-		  }
-		});
+	if (navigator.geolocation) {
+  		navigator.geolocation.getCurrentPosition(function(position) {
+	  		lat = position.coords.latitude;
+	  		longt = position.coords.longitude;
+	    	$("#locData").html("latitude: " + lat + "<br>longitude: " + longt);
+  		});
 	};
-	*/
-	
-	//getWeather();
 
 	toggleUnit("#togBtn");
 	getWeather();
 	
 	$("#togBtn").on("click", function(){
 		toggleUnit ((this));
+		getWeather();
     });
 	
 	
