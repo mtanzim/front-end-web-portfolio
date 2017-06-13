c98eec062b709e9888f989cc7d4d1581ebb02293
Tanzim Mokammel <mtanzim@gmail.com>
2017-06-12 23:28:27 -0400

    got the weather app functionally working

--- localWeather/getWeather.js
+++ localWeather/getWeather.js
@@ -2,63 +2,31 @@
 
 var tempUnit = ["metric", "imperial","C","F"];
 var isCel = 0;
-var lat;
-var longt;
 var owmAPI ="b0c19d689944d2473d76de9f1cc4339f";
-var debugID = document.getElementById("debug");
 
 
-
-//window.onload = loadLocation();
-
-/*
-function loadLocation (){
-	//var location=[];
-	if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function(position) {
-		  		 lat = position.coords.latitude;
-		  		 longt = position.coords.longitude;
-			});
-	} 
-}
-*/
-
-
-//function loadLocation () {
-	
-	//return coord;
-//}
 
 function getPosition (position) {
-	lat = position.coords.latitude;
-	longt = position.coords.longitude;
-	/*
 	var lat = position.coords.latitude;
 	var longt = position.coords.longitude;
-
 	var coordLoc = [];
 	coordLoc.push(lat);
 	coordLoc.push(longt);
-	return coordLoc;
-	*/
+	getWeather(coordLoc);
 }
+
 
 function toggleUnit (divInfo) {
 	if (isCel===0) {
-		//$(divInfo).html(tempUnit[isCel]);
 		isCel=1;
 	} else {
-		//$(divInfo).html(tempUnit[isCel]);
 		isCel=0;
 	}
 }
 
+
 function getWeather (location) {
-	//var coord = loadLocation();
-	//var lat = coord[0];
-	//var longt = coord[1];
-	//loadLocation();
-	$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+longt+"&APPID="+owmAPI+"&units="+tempUnit[isCel], function(json) {
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+location[0]+"&lon="+location[1]+"&APPID="+owmAPI+"&units="+tempUnit[isCel], function(json) {
   		var html = '';
   		//html += lat+","+longt+"<br>";
   		//html+=JSON.stringify(json)+"<br><br>";
@@ -70,32 +38,24 @@ function getWeather (location) {
   		html += "<img id=\'weatherIcon\' src=\'"+ imageLink+"\'><br>";
   		html+=json["main"]["temp"]+" "+tempUnit[isCel+2]+"<br>";
   		$("#weatherInfo").html(html);
-  		/*
-  		var relevantWeather = {
-  			name = json["name"];
-  		};
-  		*/
+  	
 	});
 }
 
-$(document).ready(function(){
-
-	//loadLocation();
+function initWeather () {
 	if (navigator.geolocation) {
-		//var coord = navigator.geolocation.getCurrentPosition (getPosition);
 		navigator.geolocation.getCurrentPosition (getPosition);
 	} else {
-		debugID.html("Location not supported on this browser.");
+		$("#debugDiv").html("Location not supported on this browser.");
 	}
-	getWeather();
+}
 
-	//document.getElementById("togBtn").click();
-
+$(document).ready(function(){
+	initWeather();
 	$("#togBtn").on("click", function(){
 		toggleUnit ((this));
-		getWeather();
+		initWeather();
     });
-	
 	
 });
 	
