a462ce8c33eacc49c96c8c6d8c5e1430be29b142
Mokammel <tmokamme@amd.com>
2017-07-17 19:47:20 -0400

    improved functionality, and fixed some bugs, added buttons for start, stop, reset

--- frontEndAdvanced/pomodoro/pomodoro.js
+++ frontEndAdvanced/pomodoro/pomodoro.js
@@ -1,48 +1,86 @@
+
+
+
+//found on stackoverflow
+function pad (num, size){
+	var s=num+""; //convert to string
+	while (s.length <size) {
+		s= "0"+s;
+	}
+	return s;
+}
+
+
 //main function
-
-
-
-
-
 $(document).ready(function(){
 
 
 	function pomCount(isWork, intervalID){
 
-		
-
 		var useTime=workTime;
 		var timeLabel='Work'
+		isItWork=isWork;
 		if (isWork){workTime --;}
 		else {breakTime--;useTime=breakTime;timeLabel='Break'}
+
+		var minDisp=pad(Math.floor(useTime/60),2);
+		var secDisp=pad(useTime%60,2);
 
 		console.log(timeLabel+':'+useTime);
 
 		$('#timeLabel').html(timeLabel);
 		$('#timeH').html('');
-		$('#timeH').append(Math.floor(useTime/60)+' minutes '+useTime%60+' seconds');
-		//$('#titleTime').preventDefault();
-		$('#titleTime').html(timeLabel+": ");
-		$('#titleTime').append(Math.floor(useTime/60)+':'+useTime%60);
+		$('#timeH').append(minDisp+' minutes '+secDisp+' seconds');
+		$('#titleTime').html(timeLabel+" ");
+		$('#titleTime').append(minDisp+':'+secDisp);
 
-		if (useTime===-1){
+
+		$("#stopBtn").on("click", function(){
+			prevInterval=intervalID;
+			clearInterval(intervalID);
+    	});
+		$("#resetBtn").on("click", function(){
+			clearInterval(intervalID);
+			clearInterval(prevInterval);
+			//reset clocks
+			$('#timeH').html(timeLabel + ' session reset.');
+			$('#titleTime').html('Complete');
+			workTime=1*60;
+			breakTime=1*60;
+			isItWork=true;
+
+    	});
+
+
+		if (useTime<0){
+			clearInterval(intervalID);
 			$('#timeH').html(timeLabel + ' session complete.');
 			$('#titleTime').html('Complete');
-			clearInterval(intervalID);
 			alert(timeLabel+ " Complete!");
 			if (isWork){
-				var intervalIDBreak= setInterval(function() {pomCount(false, intervalIDBreak)},delayVal);
-				
+				intervalID=setInterval(function() {pomCount(false, intervalID)},delayVal);
 			}
 		}
 	}
 
 	//harcode times for now
-	var delayVal=1000;
-	var workTime=25*60;
-	var breakTime=5*60;
+	var delayVal=100;
+	var workTime=1*60;
+	var breakTime=1*60;
+	var intervalID=0;
+	var prevInterval=0;
+	var isItWork=true;
 
-	var intervalID= setInterval(function() {pomCount(true, intervalID)},delayVal);
+
+	$("#startBtn").on("click", function(){
+		intervalID=prevInterval;
+		intervalID= setInterval(function() {pomCount(isItWork, intervalID)},delayVal);
+    });
+
+	$("#addWork").on("click", function(){
+		alert("added work")
+    });
+	
 	
 	
 
