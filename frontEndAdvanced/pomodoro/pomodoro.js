1d1a2f6c470a165d41fefebea480dc3cad4f331a
Mokammel <tmokamme@amd.com>
2017-07-17 13:59:55 -0400

    cleaned up code

--- frontEndAdvanced/pomodoro/pomodoro.js
+++ frontEndAdvanced/pomodoro/pomodoro.js
@@ -10,51 +10,37 @@ $(document).ready(function(){
 	function pomCount(isWork, intervalID){
 
 		
-		//var totalWorkTime=minutes*60;
-		//var totalBreakTime=breakTime*60;
+
 		var useTime=workTime;
 		var timeLabel='Work'
 		if (isWork){workTime --;}
 		else {breakTime--;useTime=breakTime;timeLabel='Break'}
 
-		//console.log('came here');
 		console.log(timeLabel+':'+useTime);
 
 		$('#timeLabel').html(timeLabel);
 		$('#timeH').html('');
 		$('#timeH').append(Math.floor(useTime/60)+' minutes '+useTime%60+' seconds');
-		$('#titleTime').html('');
+		//$('#titleTime').preventDefault();
+		$('#titleTime').html(timeLabel+": ");
 		$('#titleTime').append(Math.floor(useTime/60)+':'+useTime%60);
 
 		if (useTime===-1){
+			$('#timeH').html(timeLabel + ' session complete.');
+			$('#titleTime').html('Complete');
 			clearInterval(intervalID);
-			if (breakTime===-1){$('#timeH').html('Time Expired');}
-			else{$('#timeH').html('');}
-			alert("Time Expired");
+			alert(timeLabel+ " Complete!");
 			if (isWork){
 				var intervalIDBreak= setInterval(function() {pomCount(false, intervalIDBreak)},delayVal);
-				//isWork=false;
-				/*
-				if (breakTime===-1){
-					clearInterval(intervalID);
-					$('#timeH').html('Time Expired');
-					alert("Time Expired");
-					isWork=false;
-				}
-				*/
 				
 			}
-
 		}
-
 	}
 
-	//alert("HI!");
 	//harcode times for now
 	var delayVal=1000;
 	var workTime=25*60;
 	var breakTime=5*60;
-
 
 	var intervalID= setInterval(function() {pomCount(true, intervalID)},delayVal);
 	
