33e5c3002c3647b9e1683f7d727f433a9114d587
Tanzim Mokammel <mtanzim@gmail.com>
2017-07-21 10:27:59 -0400

    adding fade functions, functionality upgrades

--- frontEndAdvanced/pomodoro/pomodoro.js
+++ frontEndAdvanced/pomodoro/pomodoro.js
@@ -10,17 +10,67 @@ function pad (num, size){
 	return s;
 }
 
+function toggleButtons(state){
+
+	if (state==='running'){
+		
+		$("#startBtn").fadeOut("slow");
+		$("#stopBtn").fadeIn("slow");
+		$("#workCtrlDiv").fadeTo("slow",0);
+		$("#breakCtrlDiv").fadeTo("slow",0);
+	}
+	else if (state==='stopped'){
+		$("#startBtn").fadeIn("slow");
+		$("#stopBtn").fadeOut("slow");
+		$("#resetBtn").fadeIn("slow");
+		$("#workCtrlDiv").fadeTo("fast",0);
+		$("#breakCtrlDiv").fadeTo("fast",0);
+	}
+	else if (state==='init'){
+		//$("#startBtn").show();
+		$("#stopBtn").hide();
+		$("#resetBtn").hide();
+	}
+	else if (state==='reset'){
+		$("#startBtn").fadeIn("slow");
+		$("#stopBtn").fadeOut("slow");
+		$("#resetBtn").fadeOut("slow");
+		$("#workCtrlDiv").fadeTo("fast",1);
+		$("#breakCtrlDiv").fadeTo("fast",1);
+	}
+}
+
 
 //main function
 $(document).ready(function(){
 
+	function reset () {
+		isItWork=true;
+		workTime=10*60;
+		breakTime=10*60;
+		toggleButtons('reset');
+		$("#startBtn").removeClass("disabled");
+		isDisabled=false;
+		pomCount(intervalID,'reset');
+		$('#titleTime').html('Complete');
+		//this is fucking up operations
+	}
 
-	function pomCount(isWork, intervalID){
+
+	function pomCount(intervalIDfunc, pomState){
+
+		if (pomState==='reset'){
+			clearInterval(intervalID);
+			clearInterval(prevInterval);
+			$('#timeH').html(timeLabel + ' session reset.');
+		} else if (pomState==='stop') {
+			prevInterval=intervalIDfunc;
+			clearInterval(intervalIDfunc);
+    	}
 
 		var useTime=workTime;
 		var timeLabel='Work'
-		isItWork=isWork;
-		if (isWork){workTime --;}
+		if (isItWork){workTime --;}
 		else {breakTime--;useTime=breakTime;timeLabel='Break'}
 
 		var minDisp=pad(Math.floor(useTime/60),2);
@@ -28,70 +78,70 @@ $(document).ready(function(){
 
 		//console.log(timeLabel+':'+useTime);
 
+
 		$('#timeLabel').html(timeLabel);
 		$('#timeH').html('');
 		$('#timeH').append(minDisp+' minutes '+secDisp+' seconds');
 		$('#titleTime').html(timeLabel+" ");
 		$('#titleTime').append(minDisp+':'+secDisp);
 
-
-		$("#stopBtn").on("click", function(){
-			$("#startBtn").removeClass("disabled");
-			isDisabled=false;
-			prevInterval=intervalID;
-			clearInterval(intervalID);
-    	});
-		$("#resetBtn").on("click", function(){
-			delayVal=10;
-			$("#startBtn").removeClass("disabled");
-			isDisabled=false;
-			clearInterval(intervalID);
-			clearInterval(prevInterval);
-			//reset clocks
-			$('#timeH').html(timeLabel + ' session reset.');
-			$('#titleTime').html('Complete');
-			workTime=1*60;
-			breakTime=1*60;
-			isItWork=true;
-
-    	});
-
-
 		if (useTime<0){
 			clearInterval(intervalID);
 			$('#timeH').html(timeLabel + ' session complete.');
 			$('#titleTime').html('Complete');
 			alert(timeLabel+ " Complete!");
-			if (isWork){
-				intervalID=setInterval(function() {pomCount(false, intervalID)},delayVal);
+			if (isItWork){
+				isItWork=false;
+				clearInterval(intervalIDfunc);
+				intervalID=setInterval(function() {pomCount(intervalIDfunc,'default')},delayVal);
+			} else {
+				reset();
 			}
 		}
 	}
 
 	//harcode times for now
-	var delayVal=1000;
-	var workTime=25*60;
-	var breakTime=5*60;
+	var delayVal=10;
+	var workTime=100*60;
+	var breakTime=150*60;
 	var intervalID=0;
 	var prevInterval=0;
 	var isItWork=true;
 
 
 	var isDisabled = document.getElementById("startBtn").disabled;
-	
+	console.log($("#workLength").text());
+	//console.log($("#workLength").text());
+	toggleButtons('init');
 
 
 	$("#startBtn").on("click", function(){
-		delayVal=1000;
+		toggleButtons('running');
 		console.log(isDisabled)
+
 		if (isDisabled===false){
 			$("#startBtn").addClass("disabled");
 			isDisabled=true;
 			intervalID=prevInterval;
-			intervalID= setInterval(function() {pomCount(isItWork, intervalID)},delayVal);
+			intervalID= setInterval(function() {pomCount(intervalID, false)},delayVal);
 		}
 		
     });
+	$("#stopBtn").on("click", function(){
+		toggleButtons('stopped');
+		pomCount(intervalID,'stop');
+		$("#startBtn").removeClass("disabled");
+		isDisabled=false;
+
+	});
+
+	$("#resetBtn").on("click", function(){
+		//delayVal=10; //speed things up
+		reset();
+
+
+
+	});
     
 
 	$("#addWork").on("click", function(){
