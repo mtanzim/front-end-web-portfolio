ff5de2f14dad85c2b4c9573f8ab59b5d072ded46
Tanzim Mokammel <mtanzim@gmail.com>
2017-07-27 19:20:02 -0400

    adding calculator project

---
+++ frontEndAdvanced/calculator/calculator.js
@@ -0,0 +1,21 @@
+
+
+
+//found on stackoverflow
+function pad (num, size){
+	var s=num+""; //convert to string
+	while (s.length <size) {
+		s= "0"+s;
+	}
+	return s;
+}
+
+
+
+
+//main function
+$(document).ready(function(){
+
+	//alert ("Hi, I'm the calculator!")	
+
+});
