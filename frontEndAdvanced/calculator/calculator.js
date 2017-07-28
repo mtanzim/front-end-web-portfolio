7f15389de3e3377fba4a25cc4b06e7b7d67c7760
Tanzim Mokammel <mtanzim@gmail.com>
2017-07-28 18:10:10 -0400

    added buttons to the calculator

--- frontEndAdvanced/calculator/calculator.js
+++ frontEndAdvanced/calculator/calculator.js
@@ -16,6 +16,19 @@ function pad (num, size){
 //main function
 $(document).ready(function(){
 
-	//alert ("Hi, I'm the calculator!")	
+	//alert ("Hi, I'm the calculator!")
+	var calcOrder=['AC','CE','/', 'x',7,8,9,'-',4,5,6,'+',1,2,3,0,'.','=' ];
+	var bootstrapColWidth="col-md-3"
+	for (var i in calcOrder){
+		if (calcOrder[i]==='='){
+			bootstrapColWidth="col-md-9"
+		} else {bootstrapColWidth="col-md-3";}
+		$('#calcButtons').append('<div class="'+bootstrapColWidth+'"" text-center"><button type="button" id="btn_'+calcOrder+'"class="btn btn-primary btn-lg">'+calcOrder[i]+'</button></div>');
+		if (['x', '-', '+',0].includes(calcOrder[i])){
+			$('#calcButtons').append('<div class="col-md-12"><br></div>')
+		}
+
+	}
+		
 
 });
