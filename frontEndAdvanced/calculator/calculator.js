9f4e194d0b5373c8c6d71308590ec3f6928f736f
Tanzim Mokammel <mtanzim@gmail.com>
2017-07-29 18:19:43 -0400

    started forming equations

--- frontEndAdvanced/calculator/calculator.js
+++ frontEndAdvanced/calculator/calculator.js
@@ -11,24 +11,64 @@ function pad (num, size){
 }
 
 
+function prepareButtons () {
+	var calcOrder=['AC','CE','/', '*',7,8,9,'-',4,5,6,'+',1,2,3,'.',0,'=' ];
+	var bootstrapColWidth="col-md-3"
+	var buttonList=[];
+	for (var i in calcOrder){
+		var curButton='btn_'+calcOrder[i];
+		if (calcOrder[i]==='='){
+			bootstrapColWidth="col-md-9"
+		} else {bootstrapColWidth="col-md-3";}
+		$('#calcButtons').append('<div class="'+bootstrapColWidth+'"" text-center"><button type="button" id="'+curButton+'"class="calcBtns btn btn-primary btn-lg ">'+calcOrder[i]+'</button></div>');
+		if (['*', '-', '+','.'].includes(calcOrder[i])){
+			$('#calcButtons').append('<div class="col-md-12"><br></div>')
+		}
+		buttonList.push(curButton);
+	}
+	return buttonList;
+
+}
+
+function useButton (btnID, equationFunc){
+	//alert (btnID);
+	var numButtons=['0','1','2','3','4','5','6','7','8','9','.'];
+	var opButtons=['/', 'x','-','+'];
+	var ctrlButtons=['AC','CE','='];
+	var equationToken=btnID.split('_');
+	var lastChar=equationFunc[equationFunc.length-1]
+	equationToken=equationToken[equationToken.length-1];
+
+
+	if (numButtons.includes(equationToken)){
+		equationFunc+=equationToken;
+	}
+	else if (opButtons.includes(equationToken)){
+		if (lastChar!==equationToken){
+			equationFunc+=equationToken;
+		}
+	}
+	else if (equationToken==='AC'){
+		equationFunc='';
+	}
+	
+	console.log(equationFunc);
+	return equationFunc;
+
+}
+
 
 
 //main function
 $(document).ready(function(){
 
 	//alert ("Hi, I'm the calculator!")
-	var calcOrder=['AC','CE','/', 'x',7,8,9,'-',4,5,6,'+',1,2,3,0,'.','=' ];
-	var bootstrapColWidth="col-md-3"
-	for (var i in calcOrder){
-		if (calcOrder[i]==='='){
-			bootstrapColWidth="col-md-9"
-		} else {bootstrapColWidth="col-md-3";}
-		$('#calcButtons').append('<div class="'+bootstrapColWidth+'"" text-center"><button type="button" id="btn_'+calcOrder+'"class="btn btn-primary btn-lg">'+calcOrder[i]+'</button></div>');
-		if (['x', '-', '+',0].includes(calcOrder[i])){
-			$('#calcButtons').append('<div class="col-md-12"><br></div>')
-		}
-
-	}
+	var buttons=prepareButtons();
+	var equation='';
+	console.log(buttons);	
 		
-
+	$(".calcBtns").on("click", function(){
+		equation=useButton(this.id,equation);
+		console.log(equation);
+	});
 });
