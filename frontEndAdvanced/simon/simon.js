f960fad59e7053ecd48d2871a1e73c27d312fe5e
Tanzim Mokammel <mtanzim@gmail.com>
2017-08-03 18:18:25 -0400

    added simon game

---
+++ frontEndAdvanced/simon/simon.js
@@ -0,0 +1,45 @@
+
+
+
+//found on stackoverflow
+function pad (num, size){
+	var s=num+""; //convert to string
+	while (s.length <size) {
+		s= "0"+s;
+	}
+	return s;
+}
+
+
+
+function prepareSimonButtons () {
+	var simonButtons=['g','r','b','y'];
+	//var bootstrapColWidth="col-md-3"
+	var buttonList=[];
+
+	//$('#simonButtons').append('<div class="col-md-3 btn-group">');
+	//$('#simonButtons').append('<div class="row">');	
+	for (var i in simonButtons){
+		var curButton='btn_'+simonButtons[i];
+		//$('#simonButtons').append('<div class="'+bootstrapColWidth+'"><button type="button" id="'+curButton+'" class="btn btn-primary btn-lg">'+simonButtons[i]+'</button></div>');
+		$('#simonButtons').append('<button type="button" id="'+curButton+'" class="btn btn-primary simonBigBtn"></button>');
+		//$('#simonButtons').append('<div class="col-md-12"><br></div>');
+		buttonList.push(curButton);
+	}
+	//$('#simonButtons').append('</div>');
+	$('#simonButtons').append('</div>');
+	//console.log(buttonList);
+	return buttonList;
+
+}
+
+
+//main function
+$(document).ready(function(){
+
+	// /alert ("Hi, I'm the Simon game!");
+
+	var buttons=prepareSimonButtons();
+	console.log(buttons);
+
+});
