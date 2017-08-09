6578a189c7c26967e65181459153e49988656c12
Tanzim Mokammel <mtanzim@gmail.com>
2017-08-09 01:22:46 -0400

    working on sound for simon

--- frontEndAdvanced/simon/simon.js
+++ frontEndAdvanced/simon/simon.js
@@ -13,14 +13,16 @@ function pad (num, size){
 
 
 function prepareSimonButtons () {
-	var simonButtons=['g','r','b','y'];
+	//var simonButtons=['g','r','b','y'];
 	//var bootstrapColWidth="col-md-3"
 	var buttonList=[];
+	console.log(Object.keys(simonButtons));
 
 	//$('#simonButtons').append('<div class="col-md-3 btn-group">');
 	//$('#simonButtons').append('<div class="row">');	
-	for (var i in simonButtons){
-		var curButton='btn_'+simonButtons[i];
+	for (var i in Object.keys(simonButtons)){
+		//console.log(Object.keys(simonButtons)[i]);
+		var curButton='btn_'+Object.keys(simonButtons)[i];
 		//$('#simonButtons').append('<div class="'+bootstrapColWidth+'"><button type="button" id="'+curButton+'" class="btn btn-primary btn-lg">'+simonButtons[i]+'</button></div>');
 		$('#simonButtons').append('<button type="button" id="'+curButton+'" class="btn btn-primary simonBigBtn"></button>');
 		//$('#simonButtons').append('<div class="col-md-12"><br></div>');
@@ -55,28 +57,89 @@ function togglePower (isOn, btnID, startID){
 
 }
 
+function getUserInput (count){
+	alert("awaiting input on count: "+count);
+	return true;
+}
+
+function playAsound (sequence, seqKeys, intervalID){
+	if (sequence.length>0){
+		console.log ('start '+sequence);
+		console.log ('playing sound:'+seqKeys[sequence[0]]);
+		new Audio(simonButtons[seqKeys[sequence[0]]]).play();
+		sequence.shift();
+		console.log ('end' + sequence);
+		playAsound(sequence, seqKeys, intervalID);
+	} else {
+		clearInterval(intervalID);
+	}
+
+}
+
+function playSounds (sequence){
+
+	var seqKeys = Object.keys(simonButtons);
+	var delayVal=2000;
+	//console.log(sequence);
+	//console.log(seqKeys);
+
+	var intervalID=setInterval(function() {playAsound(sequence, seqKeys, intervalID)},delayVal);
+
+	if (intervalID===0){
+		return 0;
+	} else {
+		console.log('waiting, intervalID: '+intervalID)
+	}
+
+	/*
+	for (var i in sequence){
+		alert ('playing sound:'+seqKeys[sequence[i]])	
+		new Audio(simonButtons[seqKeys[sequence[i]]]).play()
+	}
+	*/
+
+}
+
 function generateSeries(count, textID, limit){
 	//alert ("Game Started");
 	$('#'+textID).val(count);
+	var randSequence=[];
 	//play random sequence
 	for (var i=0; i < count; i++){	
-		alert (i);//this will be a random number leading to random button presses
+		//alert (i);//this will be a random number leading to random button presses
+		randSequence.push(Math.floor(Math.random() * 4));
+		
 	}
+
+	console.log(randSequence);
+	playSounds(randSequence);
 	//await input from user
-	alert("awaiting input");
+	var isCorrect=getUserInput(count);
+
 	//repeat game x number of times
-	if (count < limit){
+	if (count < limit && isCorrect){
 		generateSeries(count+1,textID, limit);
+	} else if (isCorrect===false) {
+		generateSeries(count,textID, limit);
 	}
 }
+
+//global buttons list
+var simonButtons={'g':'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
+				  'r':'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+				  'b':'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
+				  'y':'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'};
+
+
 
 //main function
 $(document).ready(function(){
 
 	var isOn=togglePower(true, 'pwrBtn','startBtn');
-	var COUNT_LIMIT=7;
+	var COUNT_LIMIT=5;
 
 	// /alert ("Hi, I'm the Simon game!");
+	console.log(simonButtons);
 
 	var buttons=prepareSimonButtons();
 	console.log(buttons);
