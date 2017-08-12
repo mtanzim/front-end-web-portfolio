e7f8e2609af9987c487f1a88aa0669dbf13aac0c
Tanzim Mokammel <mtanzim@gmail.com>
2017-08-11 21:11:22 -0400

    refactor code to put global variables in an object; on way to re-organize state machine

--- frontEndAdvanced/simon/simon.js
+++ frontEndAdvanced/simon/simon.js
@@ -16,13 +16,13 @@ function prepareSimonButtons () {
 	//var simonButtons=['g','r','b','y'];
 	//var bootstrapColWidth="col-md-3"
 	var buttonList=[];
-	console.log(Object.keys(simonButtons));
+	console.log(Object.keys(globalSimonVars.simonButtons));
 
 	//$('#simonButtons').append('<div class="col-md-3 btn-group">');
 	//$('#simonButtons').append('<div class="row">');	
-	for (var i in Object.keys(simonButtons)){
+	for (var i in Object.keys(globalSimonVars.simonButtons)){
 		//console.log(Object.keys(simonButtons)[i]);
-		var curButton='btn_'+Object.keys(simonButtons)[i];
+		var curButton='btn_'+Object.keys(globalSimonVars.simonButtons)[i];
 		//$('#simonButtons').append('<div class="'+bootstrapColWidth+'"><button type="button" id="'+curButton+'" class="btn btn-primary btn-lg">'+simonButtons[i]+'</button></div>');
 		$('#simonButtons').append('<button type="button" id="'+curButton+'" class="btn btn-primary simonBigBtn"></button>');
 		//$('#simonButtons').append('<div class="col-md-12"><br></div>');
@@ -36,7 +36,7 @@ function prepareSimonButtons () {
 }
 
 
-function togglePower (isOn, btnID, startID){
+function togglePower (isOn, btnID, startID, strictID){
 	//console.log(btnID	);
 
 	if (isOn){
@@ -45,6 +45,7 @@ function togglePower (isOn, btnID, startID){
 		$('#'+btnID).addClass("btn-danger");
 		$('#'+btnID).html("OFF");
 		$('#'+startID).prop("disabled",true);
+		$('#'+strictID).prop("disabled",true);
 	}
 	else {
 		isOn=true;	
@@ -52,26 +53,51 @@ function togglePower (isOn, btnID, startID){
 		$('#'+btnID).addClass("btn-success");
 		$('#'+btnID).html("ON");
 		$('#'+startID).prop("disabled",false);
+		$('#'+strictID).prop("disabled",false);
 	}
 	return isOn;
 
 }
 
+/*
 function getUserInput (count, sequence, callback){
+
+
+	var isChecked = $('#isStrict').is(':checked');
+	var isCorrect=true;
+	console.log ("Checkbox is checked? "+ isChecked);
 	console.log ("Need to match the following sequence: "+sequence);
-	alert("awaiting input on count: "+count+ " for the following keys: "+ sequence);
-	callback(true);
+	//alert("awaiting input on count: "+count+ " for the following keys: "+ sequence);
+	//site needs to go to a state where it accepts input
+	isInputReq=true;
+	var btn_name=seqKeys[sequence[0]];
+
+	//site needs to be in this state until sequence is complete, or an incorrect value is pressred 
+	if (sequence.length > 0){
+		if (lastBtn===btn_name){
+			sequence.shift();
+			callback(true);
+		} else {
+			 getUserInput (count, sequence, callback);
+		}
+	} else {
+		isInputReq=true;
+		callback();
+	}
+	
 }
+*/
 
 function playAsound (sequence, intervalID,callback){
 	$('.simonBigBtn').css("opacity",'1.0');
 	console.log();
 	if (sequence.length>0){
+		var seqKeys = Object.keys(globalSimonVars.simonButtons);
 		var btn_name=seqKeys[sequence[0]];
 
 		console.log ('start '+sequence);
 		console.log ('playing sound:'+btn_name);
-		new Audio(simonButtons[btn_name]).play();
+		new Audio(globalSimonVars.simonButtons[btn_name]).play();
 		$('#btn_'+btn_name).css("opacity",'0.5');
 
 
@@ -79,6 +105,7 @@ function playAsound (sequence, intervalID,callback){
 		console.log ('end' + sequence);
 	} else {
 		clearInterval(intervalID);
+		globalSimonVars.isInputReq=true;
 		callback();
 	}
 
@@ -103,8 +130,9 @@ function generateSeries(count, textID, limit){
 
 	var intervalID=setInterval(function() {playAsound(randSequence, intervalID, function () {
 		//await input from user
-		console.log ("now getting input!");
-		console.log (storeRand);
+		console.log ("now getting input? "+globalSimonVars.isInputReq);
+		console.log (storeRand);	
+		/*
 		getUserInput(count, storeRand, function(isCorrect){
 			//repeat game x number of times
 			//need this in a callback as well
@@ -114,26 +142,53 @@ function generateSeries(count, textID, limit){
 				generateSeries(count,textID, limit);
 			}
 		});
+		*/
 
 
 
 
 
-	})},DELAY_VAL);
+	})},globalSimonVars.DELAY_VAL);
 
 	console.log(randSequence);
 
 }
 
-//global buttons list
-var simonButtons={'g':'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
-				  'r':'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
-				  'b':'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
-				  'y':'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'};
 
-var seqKeys = Object.keys(simonButtons);
-var DELAY_VAL=1000;
-var COUNT_LIMIT=10;
+//global buttons list
+//need to place in global class
+
+var globalSimonVars = new function(){
+
+
+	this.simonButtons={'g':'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
+					  'r':'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+					  'b':'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
+					  'y':'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'};
+
+	this.DELAY_VAL=1000;
+	this.COUNT_LIMIT=20;
+	this.isInputReq=false;
+	this.lastBtn='';
+	this.currentStage=5;
+}
+
+
+//old global variables
+/*
+	var simonButtons={'g':'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
+					  'r':'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+					  'b':'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
+					  'y':'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'};
+
+	var seqKeys = Object.keys(simonButtons);
+	var DELAY_VAL=1000;
+	var COUNT_LIMIT=3;
+	var isInputReq=false;
+	var lastBtn='';
+	var currentStage=10;
+*/
+
 
 //main function
 $(document).ready(function(){
@@ -142,22 +197,30 @@ $(document).ready(function(){
 	
 
 	// /alert ("Hi, I'm the Simon game!");
-	console.log(simonButtons);
+	console.log(globalSimonVars.simonButtons);
 
 	var buttons=prepareSimonButtons();
 	console.log(buttons);
 
 	$("#pwrBtn").on("click", function(){
-		isOn=togglePower(isOn, this.id, 'startBtn');
+		isOn=togglePower(isOn, this.id, 'startBtn','isStrict');
 		//console.log(isOn);
 	});
 
 	$("#startBtn").on("click", function(){
 		$('#stageCount').val('--');
 		$('#'+this.id).prop("disabled",true);
-		generateSeries(1,'stageCount', COUNT_LIMIT);
+		$('#isStrict').prop("disabled", true);
+		generateSeries(globalSimonVars.currentStage,'stageCount', globalSimonVars.COUNT_LIMIT);
 		//generateSeries(1,'stageCount');
 		
 	});
+
+	for (button in buttons){
+		$(buttons).on("click", function(){
+			globalSimonVars.lastBtn='#'+this.id;
+			alert(globalSimonVars.lastBtn);
+		});
+	}
 
 });
