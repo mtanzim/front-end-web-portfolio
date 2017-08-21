682e471a14b99cda5cf0c9d633cd78a354b327ec
Tanzim Mokammel <mtanzim@gmail.com>
2017-08-21 00:24:09 -0400

    made buttons responsive

--- frontEndAdvanced/tictactoe/tictactoe.js
+++ frontEndAdvanced/tictactoe/tictactoe.js
@@ -12,15 +12,15 @@ function pad (num, size){
 
 
 //creates a grid of buttons
-function prepareButtons (divName) {
+function prepareGameBoard (divName) {
 
 	var buttonList=[];
 	var bootstrapColWidth="col-md-4 col-sm-4 col-xs-4"
 	var gridOrder=[0,1,2,3,4,5,6,7,8];
-	var btnClass="ticBtns";
+	var btnClass="";
 	for (var i in gridOrder){
 		var curButton='btn_'+gridOrder[i];
-		$(divName).append('<div class="'+bootstrapColWidth+'"" text-center"><button type="button" id="'+curButton+'"class="'+btnClass+'" btn btn-primary btn-lg ">'+''+'</button></div>');
+		$(divName).append('<div class="'+bootstrapColWidth+'"" text-center"><button type="button" id="'+curButton+'"class="'+btnClass+'well btn btn-default">'+''+'</button></div>');
 		buttonList.push(curButton);
 	}
 	return buttonList;
@@ -34,8 +34,16 @@ $(document).ready(function(){
 
 	//alert ("Hi, I'm Tic Tac Toe!");
 	var gameDiv="#gameArea";
-	var buttonList=prepareButtons(gameDiv);
+	var buttonList=prepareGameBoard(gameDiv);
 	console.log(buttonList);
+
+	for (btnID in buttonList){
+		console.log(buttonList[btnID]);
+		$('#'+buttonList[btnID]).on("click", function(){
+			alert(this.id);
+		});
+
+	}
 	//$(gameDiv).append('<div class="row text-center"><div class="col-md-4 col-md-offset-4"><p>Hello');
 
 
